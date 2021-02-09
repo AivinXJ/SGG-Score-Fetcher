3730b0b90958f6242034f43968e8ff5d9baa4a6a
Aivin <61160555+AivinXJ@users.noreply.github.com>
2021-02-09 08:24:35 +0300

    Update installer.py

--- installer.py
+++ installer.py
@@ -1,4 +1,5 @@
 import os
-os.system('pip install sgqlc')
-os.system('pip install Pillow')
-os.system('pip install requests')
+with open('requirements.txt', 'w') as f:
+  f.write('sgqlc\nPillow\requests')
+os.system('pip install -r requirements.txt')
+os.remove('requirements.txt')
