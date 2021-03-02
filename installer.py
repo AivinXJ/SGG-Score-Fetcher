d95835257601aa188f32d716cbb84c69d1c6af2c
Aivin <61160555+AivinXJ@users.noreply.github.com>
2021-03-02 12:51:15 +0300

    Update installer.py

--- installer.py
+++ installer.py
@@ -1,5 +1,4 @@
 import os
-def install():
-    os.system('pip install Pillow')
-    os.system('pip install requests')
-    os.system('pip install sgqlc')
+os.system('pip install Pillow')
+os.system('pip install requests')
+os.system('pip install sgqlc')
